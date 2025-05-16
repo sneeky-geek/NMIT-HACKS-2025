fe141a8e70f2e253f0222c03abe35f597e01ebae
sneeky-geek <sharadsr69@gmail.com>
2025-05-16 16:34:43 +0530

    initialisation of backend

---
+++ Server/index.js
@@ -0,0 +1,14 @@
+// Basic Express server setup
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
